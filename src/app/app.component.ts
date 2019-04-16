149851ce6391040d2a3588eb16a0b67deabdca4e
Rico Barnes <rico.barnes@cloverbyte.com>
2019-04-16 00:35:50 -0400

    setup query change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'example';
+  colors = ['Blue', 'Green', 'Red', 'Yellow'];
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
+
+  setQuery(str: string) {
+
+    console.log('color', str);
+
+    this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams: { color: str }
+    });
+  }
+
 }
